d5903db8be30d134b8a857f68ed9e44f223aea79
Toph Lumina <2625792753@qq.com>
2026-03-18 18:55:00 +1100

    delegate fin

--- Delegates/Delegate.h
+++ Delegates/Delegate.h
@@ -13,13 +13,15 @@ template <typename... Args> class Delegate
 
   private:
     std::vector<std::pair<DelegateHandle, std::function<void(Args...)>>> functions;
-    std::mutex mutex;
+    mutable std::mutex mutex;
     DelegateHandle nextHandle = 1;
 
   public:
     Delegate() = default;
     Delegate(const Delegate &other) = delete;
     Delegate &operator=(const Delegate &other) = delete;
+    Delegate(Delegate &&) noexcept = default;
+    Delegate &operator=(Delegate &&) noexcept = default;
     ~Delegate() = default;
 
     DelegateHandle operator+=(const std::function<void(Args...)> &func)
@@ -32,9 +34,22 @@ template <typename... Args> class Delegate
     void operator-=(DelegateHandle handle)
     {
         std::lock_guard<std::mutex> lock(mutex);
-        functions.erase(std::remove_if(functions.begin(), functions.end(),
-                                       [handle](const auto &pair) { return pair.first == handle; }),
+        functions.erase(std::remove_if(functions, [handle](const auto &pair) { return pair.first == handle; }),
                         functions.end());
+    }
+
+    std::vector<DelegateHandle> operator+=(const Delegate &other)
+    {
+        std::vector<DelegateHandle> handles;
+        if (this == &other)
+            return handles; // Avoid self-assignment
+        std::scoped_lock lock(mutex, other.mutex);
+        for (const auto &pair : other.functions)
+        {
+            functions.push_back(pair); // Add each function from the other delegate
+            handles.push_back(pair.first);
+        }
+        return handles;
     }
 
     void Clear()
